30362477a0f855de12043aaa0f472ef4dc70f715
7shi <7shi@live.jp>
2011-04-22 14:11:17 +0900

    空のEXEファイルを出力（実行不可）

--- 04/04/Module1.vb
+++ 04/04/Module1.vb
@@ -1,7 +1,12 @@
-﻿Module Module1
+﻿Imports System.IO
+
+Module Module1
 
     Sub Main()
-
+        Dim image(&H3FF) As Byte
+        Using fs = New FileStream("output.exe", FileMode.Create)
+            fs.Write(image, 0, image.Length)
+        End Using
     End Sub
 
 End Module
