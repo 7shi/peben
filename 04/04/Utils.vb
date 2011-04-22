420c3ef984c5b4a937338c2fd5493c512bab9d9b
7shi <7shi@live.jp>
2011-04-22 16:55:12 +0900

    関数から位置を返す

--- 04/04/Utils.vb
+++ 04/04/Utils.vb
@@ -1,10 +1,11 @@
 ﻿Imports System.Text
 
 Module Utils
-    Sub write16(image As Byte(), pos%, v As UShort)
+    Function write16%(image As Byte(), pos%, v As UShort)
         image(pos) = v And &HFF
         image(pos + 1) = v >> 8
-    End Sub
+        Return pos + 2
+    End Function
 
     Function write16s%(image As Byte(), pos%, ParamArray vs As UShort())
         For Each v In vs
@@ -14,28 +15,29 @@ Module Utils
         Return pos
     End Function
 
-    Sub write32(image As Byte(), pos%, v%)
+    Function write32%(image As Byte(), pos%, v%)
         image(pos) = v And &HFF
         image(pos + 1) = (v >> 8) And &HFF
         image(pos + 2) = (v >> 16) And &HFF
         image(pos + 3) = (v >> 24) And &HFF
-    End Sub
+        Return pos + 4
+    End Function
 
     Function write32s%(image As Byte(), pos%, ParamArray vs%())
         For Each v In vs
-            write32(image, pos, v)
-            pos += 4
+            pos = write32(image, pos, v)
         Next
         Return pos
     End Function
 
-    Sub writebin(image As Byte(), pos%, bin As Byte())
+    Function writebin%(image As Byte(), pos%, bin As Byte())
         Array.Copy(bin, 0, image, pos, bin.Length)
-    End Sub
+        Return pos + bin.Length
+    End Function
 
-    Sub writestr(image As Byte(), pos%, s$)
-        writebin(image, pos, Encoding.UTF8.GetBytes(s))
-    End Sub
+    Function writestr%(image As Byte(), pos%, s$)
+        Return writebin(image, pos, Encoding.UTF8.GetBytes(s))
+    End Function
 
     Function conv16(s$) As UShort
         Dim bin = Encoding.UTF8.GetBytes(s)
