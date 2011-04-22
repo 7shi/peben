1716cf7d59dda2954257f9a2077da4c0417c2bed
7shi <7shi@live.jp>
2011-04-22 16:37:36 +0900

    バイナリ操作関数をUtilsに分離

--- 04/04/Headers.vb
+++ 04/04/Headers.vb
@@ -4,12 +4,12 @@
         e_res(3), e_oemid, e_oeminfo, e_res2(9) As UShort
     Public e_lfanew%
 
-    Sub write(pos%)
-        pos = write16s(pos, e_magic, e_cblp, e_cp, e_crlc, e_cparhdr, e_minalloc, e_maxalloc,
-                       e_ss, e_sp, e_csum, e_ip, e_cs, e_lfarlc, e_ovno)
-        pos = write16s(pos, e_res)
-        pos = write16s(pos, e_oemid, e_oeminfo)
-        pos = write16s(pos, e_res2)
-        write32(pos, e_lfanew)
+    Sub write(image As Byte(), pos%)
+        pos = write16s(image, pos, e_magic, e_cblp, e_cp, e_crlc, e_cparhdr, e_minalloc,
+                        e_maxalloc, e_ss, e_sp, e_csum, e_ip, e_cs, e_lfarlc, e_ovno)
+        pos = write16s(image, pos, e_res)
+        pos = write16s(image, pos, e_oemid, e_oeminfo)
+        pos = write16s(image, pos, e_res2)
+        write32(image, pos, e_lfanew)
     End Sub
 End Class
---
+++ 04/04/Utils.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Text
+
+Module Utils
+    Sub write16(image As Byte(), pos%, v As UShort)
+        image(pos) = v And &HFF
+        image(pos + 1) = v >> 8
+    End Sub
+
+    Function write16s%(image As Byte(), pos%, ParamArray vs As UShort())
+        For Each v In vs
+            write16(image, pos, v)
+            pos += 2
+        Next
+        Return pos
+    End Function
+
+    Sub write32(image As Byte(), pos%, v%)
+        image(pos) = v And &HFF
+        image(pos + 1) = (v >> 8) And &HFF
+        image(pos + 2) = (v >> 16) And &HFF
+        image(pos + 3) = (v >> 24) And &HFF
+    End Sub
+
+    Sub writebin(image As Byte(), pos%, bin As Byte())
+        Array.Copy(bin, 0, image, pos, bin.Length)
+    End Sub
+
+    Sub writestr(image As Byte(), pos%, s$)
+        writebin(image, pos, Encoding.UTF8.GetBytes(s))
+    End Sub
+
+    Function conv16(s$) As UShort
+        Dim bin = Encoding.UTF8.GetBytes(s)
+        ReDim Preserve bin(1)
+        Return BitConverter.ToUInt16(bin, 0)
+    End Function
+End Module
