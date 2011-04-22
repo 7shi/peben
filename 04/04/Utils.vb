5c4714cfab88c583c2c097a2782e035f0c8dba8a
7shi <7shi@live.jp>
2011-04-22 17:20:13 +0900

    writeAnyを追加

--- 04/04/Utils.vb
+++ 04/04/Utils.vb
@@ -45,26 +45,34 @@ Module Utils
         Return BitConverter.ToUInt16(bin, 0)
     End Function
 
-    Function writeFields%(image As Byte(), pos%, obj As Object)
-        Dim t = obj.GetType
-        For Each f In t.GetFields()
-            Dim v = f.GetValue(obj)
-            If TypeOf v Is Byte Then
-                image(pos) = CByte(v)
-                pos += 1
-            ElseIf TypeOf v Is UShort Then
-                pos = write16(image, pos, CUShort(v))
-            ElseIf TypeOf v Is Integer Then
-                pos = write32(image, pos, CInt(v))
-            ElseIf TypeOf v Is Byte() Then
-                pos = write8s(image, pos, CType(v, Byte()))
-            ElseIf TypeOf v Is UShort() Then
-                pos = write16s(image, pos, CType(v, UShort()))
-            ElseIf TypeOf v Is Integer() Then
-                pos = write32s(image, pos, CType(v, Integer()))
+    Function writeAny%(image As Byte(), pos%, v As Object)
+        If TypeOf v Is Byte Then
+            image(pos) = CByte(v)
+            Return pos + 1
+        ElseIf TypeOf v Is UShort Then
+            Return write16(image, pos, CUShort(v))
+        ElseIf TypeOf v Is Integer Then
+            Return write32(image, pos, CInt(v))
+        ElseIf TypeOf v Is Byte() Then
+            Return write8s(image, pos, CType(v, Byte()))
+        ElseIf TypeOf v Is UShort() Then
+            Return write16s(image, pos, CType(v, UShort()))
+        ElseIf TypeOf v Is Integer() Then
+            Return write32s(image, pos, CType(v, Integer()))
+        Else
+            Dim t = v.GetType
+            Dim fs = t.GetFields
+            If fs.Length > 0 Then
+                Return writeFields(image, pos, v)
             Else
                 Throw New ArgumentException("不明な型: ", t.FullName)
             End If
+        End If
+    End Function
+
+    Function writeFields%(image As Byte(), pos%, obj As Object)
+        For Each f In obj.GetType.GetFields()
+            pos = writeAny(image, pos, f.GetValue(obj))
         Next
         Return pos
     End Function
